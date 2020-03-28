abb6b4b58e6275a086c955ef390f1336be0d580a
Jesper Baltzersen <jbaltzersen@gnhearing.com>
2020-03-28 23:20:53 +0100

    first commit

---
+++ GetBusy/Views/Pancake.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace GetBusy.Views
+{
+    public partial class Pancake : ContentPage
+    {
+        public Pancake()
+        {
+            InitializeComponent();
+        }
+    }
+}
